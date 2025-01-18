5777d7293edeccc14cfeefad66bbe8d116918020
DuskStar <contactspeencer@gmail.com>
2025-01-17 20:11:26 -0600

    STANBY PAGECONTACT, NEW CSS

--- frontend/src/web/views/pages/PageContact.jsx
+++ frontend/src/web/views/pages/PageContact.jsx
@@ -1,9 +1,14 @@
 // Página de contacto
 import React from "react";
+import './assets/pagecontact.css'
 
 const PageContact = () => {
   return (
-    <main>
+    <main className="main-ct">
+      <section>
+        <h1>Contacto</h1>
+        <p>Esta página de contacto aun esta en desarollo...</p>
+      </section>
     </main>
   );
 };
